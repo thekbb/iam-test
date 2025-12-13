c09e4f7bbb02bfec5972da5ab6ea8e43796334d9
Kevin Behrens <kevin@thekbb.net>
2025-12-13 15:57:17 -0600

    testing - add describe* for dynamo

--- lambda.tf
+++ lambda.tf
@@ -34,6 +34,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
       {
         Effect = "Allow"
         Action = [
+          "dynamodb:Describe*",
           "dynamodb:Get*",
           "dynamodb:List*",
           "dynamodb:Query",
