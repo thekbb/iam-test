dbfa05c3d202e835d525ee0f002b23657f162031
Kevin Behrens <kevin@thekbb.net>
2026-02-07 11:43:04 -0600

    test

--- lambda.tf
+++ lambda.tf
@@ -25,6 +25,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
       {
         Effect = "Allow"
         Action = [
+          "logs:Describe",
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents",
